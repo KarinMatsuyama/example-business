8718898d8d8172d201080bd1d611d3014c60aeef
Karin <47166424+KarinMatsuyama@users.noreply.github.com>
2020-01-01 17:45:33 -0600

    add social media logo with link (#6)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 
 import homeImage from '../assets/light-bulbs.jpg';
+import fb from '../assets/fb.svg';
+import insta from '../assets/insta.png';
+import yelp from '../assets/yelp.svg';
 import Services from './Services';
 import AboutUs from './AboutUs';
 import Reviews from './Reviews';
@@ -14,6 +17,11 @@ class HomePage extends Component {
     <Parallax className="custom-class" tagOuter="figure">
         <img className="home-img" src={homeImage} />
         <h1 className="company-name" name="home">Company Name</h1>
+        <div className="social-logo">
+          <a href="https://www.facebook.com" target="_blank"><img className="fb-logo" src={fb} /></a>
+          <a href="https://www.instagram.com" target="_blank"><img className="insta-logo" src={insta} /></a>
+          <a href="https://www.yelp.com" target="_blank"><img className="yelp-logo" src={yelp} /></a>
+        </div>
     </Parallax>
     )
   }
