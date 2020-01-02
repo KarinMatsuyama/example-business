74438a10535f0ae1e57357e3a2229a0408ff2461
garyCoffey <38114759+garyCoffey@users.noreply.github.com>
2020-01-01 18:45:27 -0600

    Adds business info api and generic about us section (#7)
    * Adds business info api and generic about us section
    
    * Update ExternalAPI.js
    
    * Update ContactUs.js
    
    Co-authored-by: Karin <47166424+KarinMatsuyama@users.noreply.github.com>

--- src/api/ExternalAPI.js
+++ src/api/ExternalAPI.js
@@ -7,6 +7,16 @@ const fetchYelpReviews = () => {
   .then((response) => response.json());
 }
 
+const fetchYelpBusinessInformation = () => {
+  return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/gdDHPx4Bicz_mcOGVw2sgg', {
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_YELP_TOKEN}`
+    },
+  })
+  .then((response) => response.json());
+}
+
 export default {
-  fetchYelpReviews: fetchYelpReviews
+  fetchYelpReviews: fetchYelpReviews,
+  fetchYelpBusinessInformation: fetchYelpBusinessInformation
 }
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import ExternalAPI from '../api/ExternalAPI.js';
+import { Button } from 'react-bootstrap';
+
 
 class ContactUs extends Component {
+  state = {
+    phone: '',
+    messaging_url: ''
+  }
 
+  componentDidMount() {
+    ExternalAPI.fetchYelpBusinessInformation()
+      .then((business) => {
+        this.setState({
+          phone: business.display_phone,
+          messaging_url: business.messaging.url
+        })
+      })
+    }
   render() {
     return (
       <div id="contact">
-        <h1>This is the contact us page!</h1>
+        <h2>Contact Us!</h2>
+        <p>Phone Number: XXXXXXXX</p>
+        <Button>Get a quote!</Button>
         <br></br>
         <br></br>
         <br></br>
