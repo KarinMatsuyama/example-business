dec3bf2f011e11a047495860b1b234869f0fe0cb
KarinMatsuyama <garycoffey@Garys-MacBook-Air.local>
2020-01-04 09:29:40 -0600

    add responsive navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,14 +6,30 @@ import logo from '../../assets/logo.png'
 import './NavBar.scss'
 
 class NavBar extends Component {
+
+  state = {
+    navExpanded: false
+  }
+
+  setNavExpanded(expanded) {
+    this.setState({ navExpanded: expanded });
+  }
+
+  closeNav() {
+    this.setState({ navExpanded: false });
+  }
+
   scrollToElem(elemName) {
+    this.closeNav();
     scroller.scrollTo(elemName, {
       delay: 300,
       smooth: true,
       offset: -85
     })
   }
+
   scrollToTop() {
+    this.closeNav();
     scroll.scrollToTop({
       delay: 300
     })
@@ -21,7 +37,9 @@ class NavBar extends Component {
 
   render() {
     return (
-      <Navbar className="nav-bar justify-content-between" fixed="top" variant="dark" expand="lg">
+      <Navbar onToggle={(expanded) => this.setNavExpanded(expanded)} 
+              expanded={this.state.navExpanded} 
+              className="nav-bar" fixed="top" variant="dark" expand="lg">
         <Navbar.Brand href="/">
           <img
           alt=""
@@ -32,13 +50,16 @@ class NavBar extends Component {
           />{' '}
           <span className="logo-text">HANDY<br />SOLUTIONS</span>
         </Navbar.Brand>
-        <Nav className="justify-content-end" id="nav-menu">
-          <Nav.Link onClick={() => this.scrollToTop()} >HOME</Nav.Link>
-          <Nav.Link onClick={() => this.scrollToElem('services')} >SERVICES</Nav.Link>
-          <Nav.Link onClick={() => this.scrollToElem('about')} >ABOUT</Nav.Link>
-          <Nav.Link onClick={() => this.scrollToElem('reviews')} >REVIEWS</Nav.Link>
-          <Nav.Link onClick={() => this.scrollToElem('contact')} >CONTACT</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+          <Nav id="nav-menu">
+            <Nav.Link onClick={() => this.scrollToTop()} >HOME</Nav.Link>
+            <Nav.Link onClick={() => this.scrollToElem('services')} >SERVICES</Nav.Link>
+            <Nav.Link onClick={() => this.scrollToElem('about')} >ABOUT</Nav.Link>
+            <Nav.Link onClick={() => this.scrollToElem('reviews')} >REVIEWS</Nav.Link>
+            <Nav.Link onClick={() => this.scrollToElem('contact')} >CONTACT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
