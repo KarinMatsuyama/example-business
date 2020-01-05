7156990cbb484b35756ba3bfa92603fcab423b05
KarinMatsuyama <garycoffey@Garys-MacBook-Air.local>
2020-01-05 15:44:14 -0600

    styled contact section

--- src/components/BusinessDetails/BusinessDetails.js
+++ src/components/BusinessDetails/BusinessDetails.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import './BusinessDetails.css'
 
 class BusinessDetails extends Component {
   render() {
     return (
-      <div className='container'>
-        <h2>Contact Us!</h2>
-        <p>Phone Number: XXXXXXXX</p>
+      <div>
+        <h3>Find Us</h3>
+        <Row>
+          <Col>
+            <p id="address">1234 N. Handy Ave.<br />
+            Chicago, IL 60603</p>
+          </Col>
+          <Col>
+            <p id="info">TEL: 123-456-7890<br />
+            info@handysolutions.com</p>
+          </Col>
+        </Row>
         <div class="mapouter">
           <div class="gmap_canvas">
             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=chicago%20il&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
--- src/components/ContactEmailForm/ContactEmailForm.js
+++ src/components/ContactEmailForm/ContactEmailForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Form} from 'react-bootstrap';
+import './ContactEmailForm.scss'
 
 class ContactUs extends Component {
   state = {
@@ -17,18 +18,20 @@ class ContactUs extends Component {
 
   render() {
     return (
-      <div className='container'>
-        <Form>
+      <div>
+        <h3>Inquiries</h3>
+        <p id="inquiry">For any inquiries or questions, please call: 123-456-7890<br />or send us an email from below</p>
+        <Form id="inquiry">
         <Form.Group controlId="emailSubject" onChange={ this.setEmailSubject }>
             <Form.Label>Email Subject</Form.Label>
-            <Form.Control as="textarea" rows="3" />
+            <Form.Control type="text" placeholder="Subject" />
           </Form.Group>
           <Form.Group controlId="emailBody" onChange={ this.setEmailBody }>
             <Form.Label>Email Body</Form.Label>
-            <Form.Control as="textarea" rows="3" />
+            <Form.Control as="textarea" rows="8" placeholder="Message" />
           </Form.Group>
           <a href={`mailto:support@example.com?subject=${ this.state.subject }&body=${ this.state.body }`} title="support@example.com">
-            <Button>Send Email</Button>
+            <Button className="send-btn">Send Email</Button>
           </a>
         </Form>
       </div>
