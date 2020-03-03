3456e0e0dbfff7332485d7d55d86b550248265bc
KarinMatsuyama <garycoffey@Garys-MacBook-Air.local>
2020-03-02 22:08:27 -0600

    adds react snapshot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
 
-ReactDOM.render(<ErrorBoundary />, document.getElementById('root'));
+render(
+  <App/>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
